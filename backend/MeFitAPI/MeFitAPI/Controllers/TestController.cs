edca1dc886cffd96e3621b0ef50bfa49cffa9c42
Robin Hellgren <robin.hellgren@outlook.com>
2021-09-01 14:26:46 +0200

    feat - API server and test endpoint created

---
+++ backend/MeFitAPI/MeFitAPI/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeFitAPI.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            var rng = new Random();
+            return "Something from the API server: " + rng.Next();
+        }
+    }
+}
