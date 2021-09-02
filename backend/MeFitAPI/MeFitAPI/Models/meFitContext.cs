14b8037c2e124ec9c513bbe97b78c550d3cc5000
robin.erik.hellgren@se.experis.com <robin_hellgren1@hotmail.com>
2021-09-02 14:51:00 +0200

    fix - added secret connection string to the project

--- backend/MeFitAPI/MeFitAPI/Models/meFitContext.cs
+++ backend/MeFitAPI/MeFitAPI/Models/meFitContext.cs
@@ -30,8 +30,7 @@ namespace MeFitAPI.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=mysqlservermefit.database.windows.net,1433;Initial Catalog=meFit;Persist Security Info=False;User ID=azureuser;Password=danlachance12212!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Configuration['Server:ServiceApiKey']");
             }
         }
 
