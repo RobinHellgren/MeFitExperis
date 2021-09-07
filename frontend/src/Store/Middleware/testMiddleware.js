756daa38321b1d6a98f031f9d741f428777f7497
Sara Söderberg <sara.soderberg@se.experis.com>
2021-09-07 10:08:56 +0200

    fix - uncommenting code

--- frontend/src/Store/Middleware/testMiddleware.js
+++ frontend/src/Store/Middleware/testMiddleware.js
@@ -1,20 +1,20 @@
 import { testActionSet, TEST_ACTION_FETCH } from "../Actions/testActions"
 const API_SERVER_CONNECTION_STRING = "http://localhost:5000"
-export const testMiddleware = ({dispatch}) => next => action => {
+export const testMiddleware = ({ dispatch }) => next => action => {
     next(action)
-    switch(action.type){
+    switch (action.type) {
         case TEST_ACTION_FETCH:
-           /* fetch(API_SERVER_CONNECTION_STRING + "/test")
-            .then(response => {
-                console.log(response)
-                dispatch(testActionSet(response))
-            })
-            .catch(error => {
-                dispatch(testActionSet(error))
-            })
+            fetch(API_SERVER_CONNECTION_STRING + "/test")
+                .then(response => {
+                    console.log(response)
+                    dispatch(testActionSet(response))
+                })
+                .catch(error => {
+                    dispatch(testActionSet(error))
+                })
             break;
 
-        default:*/
-            
+        default:
+
     }
 }
