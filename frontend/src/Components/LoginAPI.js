2b687a1e615416b84e5b86fc94b99909a32d0435
Sara Söderberg <sara.soderberg@se.experis.com>
2021-09-08 10:08:02 +0200

    fix - login fetch

--- frontend/src/Components/LoginAPI.js
+++ frontend/src/Components/LoginAPI.js
@@ -4,33 +4,28 @@ export const LoginAPI = {
     login(credentials) {
 
         var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-        var urlencoded = new URLSearchParams();
-        urlencoded.append("password", credentials.password);
-        urlencoded.append("username", credentials.username);
-        urlencoded.append("client_id", "mefit");
-        urlencoded.append("grant_type", "password");
-        urlencoded.append("client_secret", "7e4c0630-078a-4868-a9a3-df978ce6db0e");
-
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+        myHeaders.append('Access-Control-Allow-Credentials', 'true');
+        myHeaders.append('Origin','http://localhost:3000');
+        
+        
+        var raw = JSON.stringify({
+          "username": credentials.username,
+          "password": credentials.password
+        });
+        
         var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: urlencoded,
-            redirect: 'follow'
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
         };
-         
-        //key.REACT_APP_SERVER_URL + /login
-        return fetch("https://mefitkeycloak.azurewebsites.net/auth/realms/MeFit/protocol/openid-connect/token", requestOptions)
-            .then(async response => {
-                if (!response.ok) {
-                    const { error = 'Unknown error' } = await response.json()
-                    console.log(response)
-                    throw new Error(error)
-                }
-                return response.json()
-
-            })
+        
+        return fetch("https://mefitapiserver.azurewebsites.net/login", requestOptions, {mode:'cors'})
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
 
     }
 }
