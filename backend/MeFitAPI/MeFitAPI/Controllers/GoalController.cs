512e7a3b08fe6edd9ea3cf146b0c0e67e1935b5e
Robin Hellgren <robin.hellgren@outlook.com>
2021-09-09 16:42:06 +0200

    feat - added get on /goals which fetches the currently authenticated users active goals

--- backend/MeFitAPI/MeFitAPI/Controllers/GoalController.cs
+++ backend/MeFitAPI/MeFitAPI/Controllers/GoalController.cs
@@ -1,16 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MeFitAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeFitAPI.Controllers
 {
+        [Route("/goals")]
+        [ApiController]
     public class GoalController : Controller
     {
-        public IActionResult Index()
+        private readonly MeFitAPI.Models.meFitContext _meFitContext;
+        private readonly AutoMapper.IMapper _mapper;
+
+        public GoalController(MeFitAPI.Models.meFitContext context, AutoMapper.IMapper mapper)
         {
-            return View();
+            _meFitContext = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetGoals ()
+        {
+            StringValues tokenBase64;
+            HttpContext.Request.Headers.TryGetValue("Authorization",out tokenBase64);
+            var jwt = tokenBase64.ToArray()[0].Split(" ")[1];
+
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(jwt);
+            var tokenS = jsonToken as JwtSecurityToken;
+
+            var userID = tokenS.Claims.ToArray()[17].Value;
+
+            IEnumerable<Models.Goal> goals = _meFitContext.Goals
+                .Include(goal => goal.Profile)
+                .Include(goal => goal.Program)
+                .Include(goal => goal.GoalWorkouts)
+                .Where(goal => goal.Profile.UserId == userID && goal.Completed == false);
+
+            IEnumerable<Models.DTO.GoalDTO.UserProfileGoalDTO> goalsDTO = goals
+                .Select(goal => _mapper.Map<Models.Goal, Models.DTO.GoalDTO.UserProfileGoalDTO>(goal));
+            
+            return Ok(goalsDTO);
         }
     }
 }
---
+++ backend/MeFitAPI/MeFitAPI/Profiles/GoalProfile.cs
@@ -0,0 +1,16 @@
+﻿using MeFitAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeFitAPI.Profiles
+{
+    public class GoalProfile : AutoMapper.Profile
+    {
+        public GoalProfile()
+        {
+            CreateMap<Goal, Models.DTO.GoalDTO.UserProfileGoalDTO>();
+        }
+    }
+}
