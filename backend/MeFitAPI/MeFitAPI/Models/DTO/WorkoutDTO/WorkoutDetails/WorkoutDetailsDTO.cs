12a6a5fc7f6d5081d7bd0104a9dba59553b594e6
robin.erik.hellgren@se.experis.com <robin_hellgren1@hotmail.com>
2021-09-16 13:13:14 +0200

    feat - added workout-lvl and owner_id to Workout

--- backend/MeFitAPI/MeFitAPI/Models/DTO/WorkoutDTO/WorkoutDetails/WorkoutDetailsDTO.cs
+++ backend/MeFitAPI/MeFitAPI/Models/DTO/WorkoutDTO/WorkoutDetails/WorkoutDetailsDTO.cs
@@ -17,6 +17,7 @@ namespace MeFitAPI.Models.DTO.WorkoutDTO.WorkoutDetails
     public int WorkoutId { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
+    public int? WorkoutLevel { get; set; }
 
     public virtual ICollection<WorkoutSetDetailsDTO> WorkoutSets { get; set; }
     public virtual ICollection<WorkoutProgramDetailsDTO> ProgramWorkouts { get; set; }
