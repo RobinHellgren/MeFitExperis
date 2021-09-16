fe13bb8a062a459bb8ae00cd490bce890170a119
Robin Hellgren <robin.hellgren@outlook.com>
2021-09-16 12:55:32 +0200

    fix - goal now adds the relations passed from the program and the incoming dto to the database

--- backend/MeFitAPI/MeFitAPI/Models/DTO/GoalWorkoutDTO/GoalRelationDTO.cs
+++ backend/MeFitAPI/MeFitAPI/Models/DTO/GoalWorkoutDTO/GoalRelationDTO.cs
@@ -7,7 +7,6 @@ namespace MeFitAPI.Models.DTO.GoalWorkoutDTO
 {
     public class GoalRelationDTO
     {
-        public int GoalId { get; set; }
         public int WorkoutId { get; set; }
         public bool? Complete { get; set; }
     }
