ccd80ff38a1730eb116605f320f7775404ef5387
Sara Söderberg <sara.soderberg@se.experis.com>
2021-09-21 16:57:35 +0200

    fix - GetAllWorkouts working correct

--- backend/MeFitAPI/MeFitAPI/Controllers/WorkoutController.cs
+++ backend/MeFitAPI/MeFitAPI/Controllers/WorkoutController.cs
@@ -79,17 +79,17 @@ namespace MeFitAPI.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetAllWorkouts()
         {
-            var workoutList = _context.Workouts.ToList();
-
+            var workoutList = _context.Workouts
+                .Include(workout => workout.NumberOfSets)
+                    .ThenInclude(set => set.Exercise)
+                .Include(workout => workout.ProgramWorkouts)
+                    .ThenInclude(program => program.Program)
+                .ToList();
             if (workoutList.Count == 0)
             {
                 return NotFound();
             }
-
             List<Models.DTO.WorkoutDTO.WorkoutDetails.WorkoutDetailsDTO> dtoList = _mapper.Map<List<Models.DTO.WorkoutDTO.WorkoutDetails.WorkoutDetailsDTO>>(workoutList);
-
-
-
             return Ok(dtoList);
 
         }
